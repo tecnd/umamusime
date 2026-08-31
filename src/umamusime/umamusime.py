ac5b3ea1811dbad6227d97a2147031cd7b4c405c
Kerry Wang <10476898+tecnd@users.noreply.github.com>
2026-08-31 00:30:27 -0500

    initial commit

---
+++ src/umamusime/umamusime.py
@@ -0,0 +1,57 @@
+from typing import Any
+
+import pyspiel
+
+JsonDict = dict[str, Any]
+
+_GAME_TYPE = pyspiel.GameType(
+    short_name="umamusime",
+    long_name="Umamusime",
+    dynamics=pyspiel.GameType.Dynamics.SEQUENTIAL,
+    chance_mode=pyspiel.GameType.ChanceMode.EXPLICIT_STOCHASTIC,
+    information=pyspiel.GameType.Information.PERFECT_INFORMATION,
+    utility=pyspiel.GameType.Utility.GENERAL_SUM,
+    reward_model=pyspiel.GameType.RewardModel.REWARDS,
+    max_num_players=1,
+    min_num_players=1,
+    provides_information_state_string=True,
+    provides_information_state_tensor=False,
+    provides_observation_string=True,
+    provides_observation_tensor=False,
+    parameter_specification={})
+
+_GAME_INFO = pyspiel.GameInfo(
+    num_distinct_actions=6,
+    max_chance_outcomes=3,
+    num_players=1,
+    min_utility=0.0,
+    max_utility=20000.0,
+    max_game_length=60)
+
+class UmaGame(pyspiel.Game):
+    def __init__(self, params=None):
+        super().__init__(_GAME_TYPE, _GAME_INFO, params or {})
+    def new_initial_state(self, state=None):
+        return UmaState(self, state)
+
+class UmaState(pyspiel.State):
+    def __init__(self, game: UmaGame, state:pyspiel.StateStruct | JsonDict | None=None):
+        super().__init__(game)
+        self._is_chance_node = False
+        self._turn = 0
+
+    def current_player(self):
+        if self._is_chance_node:
+            return pyspiel.PlayerId.CHANCE
+        return 0
+
+    def legal_actions(self, player=None):
+        return [a for a in range(_GAME_INFO.num_distinct_actions)]
+
+    def apply_action(self, action): ...
+    def is_terminal(self):
+        return self._turn >= _GAME_INFO.max_game_length
+    def returns(self): ...
+    def __str__(self): ...
+
+pyspiel.register_game(_GAME_TYPE, UmaGame)
